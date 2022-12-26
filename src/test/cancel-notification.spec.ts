35608ac552a735b116e1a53444864d4622e9f09e
EriikGabriel <erikgabriel.lins@hotmail.com>
2022-12-25 23:55:10 -0300

    Commit - Ignite Lab Node (Complete) ✅

---
+++ src/test/cancel-notification.spec.ts
@@ -0,0 +1,34 @@
+import { NotificationNotFound } from './../app/use-cases/errors/notification-not-found';
+import { InMemoryNotificationsRepository } from './repositories/in-memory-notifications-repository';
+import { CancelNotification } from '../app/use-cases/cancel-notification';
+import { makeNotification } from './factories/notification-factory';
+
+describe('Cancel notification', () => {
+  it('should be able to cancel a notification', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const cancelNotification = new CancelNotification(notificationsRepository);
+
+    const notification = makeNotification();
+
+    await notificationsRepository.create(notification);
+
+    await cancelNotification.execute({
+      notificationId: notification.id,
+    });
+
+    expect(notificationsRepository.notifications[0].canceledAt).toEqual(
+      expect.any(Date),
+    );
+  });
+
+  it('should not be able to cancel a non existing notification', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const cancelNotification = new CancelNotification(notificationsRepository);
+
+    expect(() => {
+      return cancelNotification.execute({
+        notificationId: 'fake-notification-id',
+      });
+    }).rejects.toThrow(NotificationNotFound);
+  });
+});
---
+++ src/test/factories/notification-factory.ts
@@ -0,0 +1,16 @@
+import { Content } from '../../app/entities/content';
+import {
+  Notification,
+  NotificationProps,
+} from '../../app/entities/notification';
+
+type Override = Partial<NotificationProps>;
+
+export function makeNotification(override: Override = {}) {
+  return new Notification({
+    content: new Content('This is a notification.'),
+    category: 'social',
+    recipientId: 'recipient-id',
+    ...override,
+  });
+}
